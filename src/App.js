2bccd6c5985997af9de17706a86da52b3c679e83
hzm628 <127806300+hzm628@users.noreply.github.com>
2023-03-13 21:16:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      clicked: false
     };
   }
 
   //handler goes here
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  }
 
   render() {
     return (
       <div>
         <p>Greetings!</p>
-        <p>Button goes here</p>
+        <button onClick={this.handleClick}>
+          {this.state.clicked ? "thanks" : "click me"}
+        </button>
       </div>
     );
   }
