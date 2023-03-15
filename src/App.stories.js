ed99e1e15495ad21f8cf035e45bdbeb8272217c3
hzm628 <127806300+hzm628@users.noreply.github.com>
2023-03-14 20:34:45 -0400

    Update App.stories.js

--- src/App.stories.js
+++ src/App.stories.js
@@ -1,13 +1,13 @@
 // Stories for testing `App` go here
 // See https://storybook.js.org/docs/react/writing-stories/introduction#how-to-write-stories
 import React from 'react';
-import { App } from './App';
+import App from './App';
 
 export default {
     title: "App",
-    component: App
+    component: App,
 }
 
 const Template = args => <App {...args}/>
 
-export const App = Template.bind({})
+export const Default = Template.bind({}) 
