dbcbd94f553c6d58afbeeee504ae13c4e6674303
hzm628 <127806300+hzm628@users.noreply.github.com>
2023-03-14 21:32:22 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,19 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 
 // import your story from App.stories.js
-
+import { Default } from './App.stories';
 
 // Add your tests here
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
+test('Button should display click me initially', () => {
+    render(<Default />);
+    const button = screen.getByText(/click me/i);
+    expect(button).toBeInTheDocument();
+});
+  
+test('Button should change text to thanks on click', () => {
+    render(<Default />);
+    const button = screen.getByText(/click me/i);
+    fireEvent.click(button);
+    expect(button).toHaveTextContent(/thanks/i);
+});
